e231d02b0863a4cf3487f0375899b959078e4baf
vishaljain123 <20835799-vishaljain123@users.noreply.replit.com>
2023-07-10 17:04:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function googleSearch() {
+  var text = document.getElementById("search").value;
+  var url = 'http://www.google.com/search?q=' + text;
+
+  window.open(url, '_black')
+  document.getElementById("search").value = null
+}
+
+let search = document.getElementById('search')
+search.addEventListener('keypress', (event) => {
+  if (event.key == "Enter") {
+    googleSearch()
+  }
+})
